3cd6a7db3692effc8f50763d0578117c330a8dc5
OldBigBuddha <shinlag@outlook.jp>
2018-10-24 11:06:10 +0300

    [add]autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
+const autoprefixer = require('gulp-autoprefixer');
 const Hexo = require('hexo');
+
 const hexo = new Hexo(process.cwd(), {});
 
 gulp.task('clean', () => del(['public/**/*']));
@@ -22,13 +24,22 @@ gulp.task('generate', cb => {
   });
 });
 
+gulp.task('add-prefix', () => {
+  return gulp.src(['./public/css/**/*.css'])
+              .pipe(autoprefixer({
+                browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
+                cascade: false
+              }))
+              .pipe(gulp.dest('./public/css'))
+});
+
 gulp.task('copy', () => {
   gulp.src('./underscores/*')
   .pipe(gulp.dest("./public"));
 });
 
 gulp.task('build', function(cb) {
-  runSequence('clean', 'generate', 'copy', cb);
+  runSequence('clean', 'generate', 'add-prefix', 'copy', cb);
 });
 
 gulp.task('default', ['build']);
