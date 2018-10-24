3acccb384cfec782098aa27017f537e4ac926f1e
OldBigBuddha <shinlag@outlook.jp>
2018-10-24 09:28:09 +0300

    [add]Gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+const gulp = require('gulp');
+const del = require('del');
+const runSequence = require('run-sequence');
+const Hexo = require('hexo');
+const hexo = new Hexo(process.cwd(), {});
+
+gulp.task('clean', () => del(['public/**/*']));
+
+gulp.task('generate', cb => {
+  hexo.init().then(() => {
+      return hexo.call('generate', {
+          watch: false
+      });
+  })
+  .then(() => hexo.exit())
+  .then(() => cb())
+  .catch(err => {
+      console.log(err);
+      hexo.exit(err);
+      return cb(err);
+  });
+});
+
+gulp.task('build', function(cb) {
+  runSequence('clean', 'generate', cb);
+});
+
+gulp.task('default', ['build']);
