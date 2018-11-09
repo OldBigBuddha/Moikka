14b53db3e883e598e892cbae10e8b018faafa0c3
coreshin <corekato@gmail.com>
2018-11-09 13:39:34 +0000

    [fix]split control.js into control.js and animation.js

--- themes/brewski/source/js/control-banner.js
+++ themes/brewski/source/js/control-banner.js
@@ -1,39 +1,21 @@
 $(() => {
-  if (document.cookie.split(';').filter((item) => item.includes("privacypolicy=")).length) {}
-  else {
-    $("#cookie-banner")
-    .css("display", "block")
-    .hide()
-    .fadeIn(1500)
-    .animate({
-      "bottom": "0",
-    }, {
-      duration: 1500,
-      queue: false
-    });
-
-    $("#button-agree").on("click", () => {
-      let deadline = new Date();
-      deadline.setTime( deadline.getTime + 30*24*60*60*1000 );
-      document.cookie = "privacypolicy=agree;expires=" + deadline.toGMTString();
-      $("#cookie-banner").slideUp()
-    });
-  }
-  $(document).on({
-    "mouseenter": function(){
-      $(this).find('.description').stop().slideToggle()
-    },
-    "mouseleave": function(){
-      $(this).find('.description').stop().slideToggle()
-    }
-  }, ".post-module");
-  
-  $('#hum-menu').click(function() {
-    $(this).toggleClass('active');
-    if ($(this).hasClass('active')) {
-      $('#container').addClass('active');
-    } else {
-      $('#container').removeClass('active');
-    }
-  });
+	if (document.cookie.split(';').filter((item) => item.includes("privacypolicy=")).length) {}
+	else {
+		$("#cookie-banner")
+		.css("display", "block")
+		.hide()
+		.fadeIn(1500)
+		.animate({
+			"bottom": "0",
+		}, {
+			duration: 1500,
+			queue: false
+		});
+		$("#button-agree").on("click", () => {
+			let deadline = new Date();
+			deadline.setTime( deadline.getTime + 30*24*60*60*1000 );
+			document.cookie = "privacypolicy=agree;expires=" + deadline.toGMTString();
+			$("#cookie-banner").slideUp()
+		});
+	}
 });
